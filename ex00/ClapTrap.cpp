565702d52c92402c6a187731e9de016e8a797a4d
spenning <whimizera@mailbox.org>
2025-02-09 19:19:24 +0100

    added canonical form

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,29 +1,76 @@
 #include "ClapTrap.hpp"
 
+bool ClapTrap::isAlive()
+{
+	if (getEnergyPoints() <= 0 || getHitPoints() <= 0)
+		return (false);
+	return (true);
+}
+
 void ClapTrap::attack(const std::string& target)
 {
-	if (energyPoints <= 0 || hitPoints <= 0)
+	if (!isAlive())
 		return;
-	energyPoints -= 1;
-	std::cout << "ClapTrap " << name << " attacks " << target 
+	setEnergyPoints(getEnergyPoints() - 1);
+	std::cout << name << " attacks " << target 
 	<< ", causing " << attackDamage << " points of damage!" << std::endl;
 }
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	if (energyPoints <= 0 || hitPoints <= 0)
+	if (!isAlive())
 		return;
-	hitPoints -= amount;
-	std::cout << "ClapTrap " << name << " took " 
+	setHitPoints(getHitPoints() - static_cast<int>(amount));
+	std::cout << name << " took " 
 	 << amount << " points of damage!" << std::endl;
 }
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (energyPoints <= 0 || hitPoints <= 0)
+	if (!isAlive())
 		return;
-	energyPoints -= 1;
-	hitPoints += amount;
-	std::cout << "ClapTrap " << name << " repaired " 
+	setEnergyPoints(getEnergyPoints() - 1);
+	setHitPoints(getHitPoints() - static_cast<int>(amount));
+	std::cout << name << " repaired " 
 	 << amount << " points of hitPoints!" << std::endl;
+}
+
+std::string ClapTrap::getName() const
+{
+	return (name);
+}
+
+int ClapTrap::getHitPoints() const
+{
+	return (hitPoints);
+}
+
+int ClapTrap::getEnergyPoints() const
+{
+	return (energyPoints);
+}
+
+int ClapTrap::getAttackDamage() const
+{
+	return (attackDamage);
+}
+
+void ClapTrap::setName(std::string& title)
+{
+	name = title;
+}
+
+void ClapTrap::setAttackDamage(int val)
+{
+	attackDamage = val;
+}
+
+void ClapTrap::setEnergyPoints(int val)
+{
+	energyPoints = val;
+}
+
+void ClapTrap::setHitPoints(int val)
+{
+	hitPoints = val;
 }
 
 ClapTrap::ClapTrap(std::string title)
@@ -42,3 +89,25 @@ ClapTrap::~ClapTrap()
 	std::cout << "ClapTrap destructor called" << std::endl;
 }
 
+ClapTrap& ClapTrap::operator=(const ClapTrap& clap)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	std::string title = clap.getName();
+	if (this == &clap)
+		return (*this);
+	setName(title);
+	setAttackDamage(clap.getAttackDamage());
+	setEnergyPoints(clap.getEnergyPoints());
+	setHitPoints(clap.getHitPoints());
+	return (*this);
+}
+
+ClapTrap::ClapTrap(const ClapTrap& clap)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	std::string title = clap.getName();
+	setName(title);
+	setAttackDamage(clap.getAttackDamage());
+	setEnergyPoints(clap.getEnergyPoints());
+	setHitPoints(clap.getHitPoints());
+}
