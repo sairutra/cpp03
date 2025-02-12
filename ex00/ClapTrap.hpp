58c9d14da877f8ee2d08f2289309a3f61ee2825d
spenning <whimizera@mailbox.org>
2025-02-12 21:40:55 +0100

    changed initialization of class variables

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,9 +6,9 @@ class ClapTrap
 {
 private:
 	std::string name;
-	int hitPoints{10};
-	int energyPoints{10};
-	int attackDamage{0};
+	int hitPoints;
+	int energyPoints;
+	int attackDamage;
 public:
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,16 @@
 int main()
 {
 	ClapTrap clap("derek");
+	std::cout << clap.getAttackDamage() << std::endl;
+	std::cout << clap.getEnergyPoints() << std::endl;
+	std::cout << clap.getHitPoints() << std::endl;
 	clap.attack("some dude");
 	clap.takeDamage(5);
 	clap.beRepaired(5);
 	clap.takeDamage(10);
 	clap.takeDamage(5);
 	clap.beRepaired(5);
+	std::cout << clap.getAttackDamage() << std::endl;
+	std::cout << clap.getEnergyPoints() << std::endl;
+	std::cout << clap.getHitPoints() << std::endl;
 }
