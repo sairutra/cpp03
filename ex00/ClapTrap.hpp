57d23beebfa2696c1f5e2ce684e4cb576f2cf35f
spenning <whimizera@mailbox.org>
2025-02-12 21:28:14 +0100

    fixed ClapTrap attack damage initialization value

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -8,7 +8,7 @@ private:
 	std::string name;
 	int hitPoints{10};
 	int energyPoints{10};
-	int attackDamage{10};
+	int attackDamage{0};
 public:
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
